87fe709c4dc8b59b4fa5900c80c790c0d64d35a3
Shota Shimazu <hornet.live.mf@gmail.com>
2019-07-08 10:22:49 +0900

    revise

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "log"
+import (
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo"
+)
+
+func port() string {
+
+	port := os.Getenv("PORT")
+
+	if len(port) == 0 {
+		port = "8080"
+	}
+
+	return ":" + port
+}
 
 func main() {
-	log.Println("UNDER CONSTRUCTION")
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Main screen")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 }
